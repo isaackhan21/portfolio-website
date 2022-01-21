a18dbaef1b53610ed0aa12addfab56c3910e6c54
Awesomecoder26 <isaackhan21@gmail.com>
2022-01-21 09:19:44 +0000

    Minor changes to styling

--- components/Projects.js
+++ components/Projects.js
@@ -37,7 +37,7 @@ const ProjectCard = ({ title, link, imgUrl, number }) => {
             className="transform hover:scale-125 transition duration-2000 ease-out object-cover h-full w-full"
           />
         </div>
-        <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+        <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-blue-500 rounded-md px-2">
           {title}
         </h1>
         <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
