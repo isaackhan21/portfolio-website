1c328579f8b2bfd91d0b59444a8b5d52db8e2012
Awesomecoder26 <isaackhan21@gmail.com>
2022-01-24 16:28:33 +0000

    Made changes to Experience and AboutMe sections

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -120,13 +120,18 @@ export default function AboutMe() {
             </h1>
             <div className="flex flex-row flex-wrap mt-8">
               <img
+                src="/nextjs logo icon.png"
+                className="h-20 w-20 mx-4 my-4 bg-white"
+              />
+              <img
+                src="/tailwind.png"
+                className="h-20 w-20 mx-4 my-4 bg-white"
+              />
+              <img
                 src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"
                 className="h-20 w-20 mx-4 my-4"
               />
-              <img
-                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/java/java.png"
-                className="h-20 w-20 mx-4 my-4"
-              />
+
               <img
                 src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png"
                 className="h-20 w-20 mx-4 my-4"
@@ -139,14 +144,7 @@ export default function AboutMe() {
                 src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png"
                 className="h-20 w-20 mx-4 my-4"
               />
-              <img
-                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/angular/angular.png"
-                className="h-20 w-20 mx-4 my-4"
-              />
-              <img
-                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/vue/vue.png"
-                className="h-20 w-20 mx-4 my-4"
-              />
+
               <img
                 src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png"
                 className="h-20 w-20 mx-4 my-4"
@@ -159,14 +157,7 @@ export default function AboutMe() {
                 src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/firebase/firebase.png"
                 className="h-20 w-20 mx-4 my-4"
               />
-              <img
-                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mysql/mysql.png"
-                className="h-20 w-20 mx-4 my-4"
-              />
-              <img
-                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png"
-                className="h-20 w-20 mx-4 my-4"
-              />
+              <img src="/Material UI.png" className="h-20 w-20 mx-4 my-4" />
             </div>
           </div>
         </div>
--- constants/data.js
+++ constants/data.js
@@ -45,18 +45,25 @@ const userData = {
   ],
   about: {
     title:
-      "I'm a software developer that loves building products and web applications that impact millions of lives",
+      "I'm a web developer that loves building products and web applications that impact millions of lives",
     description: [
       `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
       `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
-      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+      `But now, I've had a vast skillset. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
     ],
     currentProject: "Crypto Price Tracker",
     currentProjectUrl: "https://reactjs-crypto-tracker.netlify.app/",
   },
   experience: [
     {
-      title: "Work Experience",
+      title: "Web Development Experience",
+      company: "Self-Taught",
+      year: "2021-2022",
+      companyLink: "https://intel.com",
+      desc: "Used online courses to develop my coding skills. Created portfolio projects in React, Next.js, Material UI and Firebase. ",
+    },
+    {
+      title: "Customer Service Experience",
       company: "Next Retail",
       year: "2017-2022",
       companyLink: "https://intel.com",
@@ -77,8 +84,7 @@ const userData = {
       desc: "Revieved GCSEs and A-levels in Maths, Physics, Chemistry",
     },
   ],
-  resumeUrl:
-    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  resumeUrl: "",
   socialLinks: {
     twitter: "https://twitter.com/Isaac77649241",
     linkedin: "https://www.linkedin.com/in/isaac-khan-2988a1159/",
