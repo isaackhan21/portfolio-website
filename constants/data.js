bbfc350c759418b577f34ff3386d6c7803d22512
Isaac Khan <isaackhan21@gmail.com>
2022-01-31 10:38:24 +0000

    Minor changes

--- constants/data.js
+++ constants/data.js
@@ -47,9 +47,9 @@ const userData = {
     title:
       "I'm a web developer that loves building products and web applications that impact millions of lives",
     description: [
-      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
-      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
-      `But now, I've had a vast skillset. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+      `Hi, I'm Isaac and I am a self-taught web developer. I didn't know what front-end or back-end meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using Material UI so that I don't have to style everything by myself and - for obvious reasons - if you knew Material UI, you were cool.`,
+      `But now, I've had a vast skillset. I've been coding in HTML, CSS, React, Next.js, JavaScript, Tailwind CSS, Node.js, Material UI, Firebase etc. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
     ],
     currentProject: "Crypto Price Tracker",
     currentProjectUrl: "https://reactjs-crypto-tracker.netlify.app/",
